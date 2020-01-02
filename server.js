efe591aeea740671826b6c7a997e76f4318d1931
zooduck <zooduck@beniceoreatsomerice.com>
2020-01-02 19:12:01 +0000

    delete unnecessary server.js

--- server.js
+++
@@ -1,5 +0,0 @@
-// const dotenv = require('dotenv');
-const express = require('express');
-const server = express();
-server.use(express.static('dist'));
-server.listen(4444, () => console.log('Listening on port: 4444'));
